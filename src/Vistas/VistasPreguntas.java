28741811a3ae13c5597a9ba6b1933f352cc5669c
Belfort1412 <cibelfort.24@est.ucab.edu.ve>
2025-05-15 19:33:29 -0400

    Se agrego funcionalidades a VistasPreguntas

--- src/Vistas/VistasPreguntas.java
+++ src/Vistas/VistasPreguntas.java
@@ -41,4 +41,62 @@ public class VistasPreguntas {
             System.out.println(preguntas[i].getIdPregunta());
         }
     }
+
+    public void modificarPregunta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el ID de la pregunta a modificar: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        Pregunta preguntaExistente = controladorPregunta.consultarPregunta(id);
+
+        if (preguntaExistente != null) {
+            System.out.println("Ingrese la nueva pregunta: ");
+            String nuevaPregunta = sc.nextLine();
+            System.out.println("Ingrese la nueva respuesta: ");
+            String nuevaRespuesta = sc.nextLine();
+
+            Pregunta nueva = new Pregunta(nuevaPregunta, nuevaRespuesta, preguntaExistente.getEstado(), preguntaExistente.getCorreoPregunta(), preguntaExistente.getCreadorPregunta(), preguntaExistente.getApruebaPregunta(), id);
+
+            controladorPregunta.modificarPregunta(nueva, preguntaExistente);
+            System.out.println("Pregunta modificada correctamente.");
+        } else {
+            System.out.println("No se encontró una pregunta con ese ID.");
+        }
+    }
+
+    public void consultarPregunta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el ID de la pregunta a consultar: ");
+        int id = sc.nextInt();
+        Pregunta pregunta = controladorPregunta.consultarPregunta(id);
+
+        if (pregunta != null) {
+            System.out.println("La pregunta es: " + pregunta.getPregunta());
+            System.out.println("La respuesta es: " + pregunta.getRespuesta());
+            System.out.println("El estado es: " + pregunta.getEstado());
+            System.out.println("El correo es: " + pregunta.getCorreoPregunta());
+            System.out.println("El creador es: " + pregunta.getCreadorPregunta());
+            System.out.println("Su Aprobacion: " + pregunta.getApruebaPregunta());
+            System.out.println("El ID: " + pregunta.getIdPregunta());
+        } else {
+            System.out.println("No se encontró una pregunta con ese ID.");
+        }
+    }
+
+    public void eliminarPregunta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el ID de la pregunta a eliminar: ");
+        int id = sc.nextInt();
+        controladorPregunta.eliminarPregunta(id);
+    }
+
+    public void aprobarPregunta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el ID de la pregunta a aprobar: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese el correo del aprobador: ");
+        String correo = sc.nextLine();
+        controladorPregunta.aprobarPregunta(id, correo);
+    }
 }
