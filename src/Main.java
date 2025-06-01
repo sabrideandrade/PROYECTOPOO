62e39b222144f73bda73125e3779c5954575b022
sabrirdeandrade <146279762+sabrideandrade@users.noreply.github.com>
2025-06-01 12:41:32 -0400

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-import Vistas.VistaMenuPrincipal;
-
-public class Main {
-    public static void main(String[] args) {
-        VistaMenuPrincipal menu = new VistaMenuPrincipal();
-        menu.mostrarMenu();
-    }
-}
