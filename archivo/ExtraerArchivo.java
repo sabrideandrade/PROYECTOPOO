abee51bdb89a03b41885e3d5497a02d7189a19e4
sabrirdeandrade <146279762+sabrideandrade@users.noreply.github.com>
2025-06-01 09:18:51 -0400

    Add files via upload

---
+++ archivo/ExtraerArchivo.java
@@ -0,0 +1,4 @@
+package archivo;
+
+public class ExtraerArchivo {
+}
---
+++ archivo/IExtraerArchivo.java
@@ -0,0 +1,6 @@
+// src/archivo/IExtraerArchivo.java
+package archivo;
+
+public interface IExtraerArchivo {
+    Object extraer(String ruta);
+}
