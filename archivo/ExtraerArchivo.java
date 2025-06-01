5cc09e0cfed9aaf04087b36a026225afd0c6d588
sabrirdeandrade <146279762+sabrideandrade@users.noreply.github.com>
2025-06-01 12:40:00 -0400

    Delete archivo directory

--- archivo/ExtraerArchivo.java
+++
@@ -1,4 +0,0 @@
-package archivo;
-
-public class ExtraerArchivo {
-}
--- archivo/IExtraerArchivo.java
+++
@@ -1,6 +0,0 @@
-// src/archivo/IExtraerArchivo.java
-package archivo;
-
-public interface IExtraerArchivo {
-    Object extraer(String ruta);
-}
